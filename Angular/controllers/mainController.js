3c184ba1443e2e1fb06e83172cd8d14860d85791
Venkatesh <venky88r@gmail.com>
2017-03-14 00:21:28 -0400

    Updated with Chunks of Code

--- Angular/controllers/mainController.js
+++ Angular/controllers/mainController.js
@@ -16,4 +16,34 @@ mainController.controller('loginController',  ['$scope', function($scope, $http)
   $scope.message = "This page will be used to display Login form";
 }]);
 
+
+mainController.controller('sliderController',  ['$scope', function($scope, $http) {
+  $scope.message = "This page will be used to display Slider form";
+}]);
+
+mainController.controller('registerController',  ['$scope', function($scope, $http) {
+  $scope.message = "Register to acces the Site";
+  $scope.register = function()
+	{
+		alert("Hi",+$scope.email);
+  		alert($scope.email.$invalid);
+	};
+}]);
+
+
+/*
+mainController.controller('accordionController',  ['$scope', function($scope, $http) {
+  $scope.message = "This page will be used to display Accordion form";
+}]);*/
+        
+mainController.controller('accordionController',  ['$scope', function($scope, $http) {
+	        var accordion = {};
+	        $scope.toggleAccordion = function(section) {
+    		$scope.accordion[section] = !$scope.accordion[section];
+}
+
+  //$scope.message = "This page will be used to display Accordion form";
+}]);
+
+
         
--- Angular/js/app.js
+++ Angular/js/app.js
@@ -1,5 +1,11 @@
 var mainApp = angular.module("mainApp", ['ngRoute','mainController']);
 
+/*mainApp.run(function($timeout){
+    $timeout(function() {
+        $(document).foundation();
+    }, 500);
+});
+*/
 mainApp.config(['$routeProvider', function($routeProvider) {
    $routeProvider.
  
@@ -17,7 +23,32 @@ mainApp.config(['$routeProvider', function($routeProvider) {
    when('/home', {
       templateUrl: 'templates/indexTemplate.html', controller: 'homeController'
    }).
- 
+
+   when('/slider', {
+      templateUrl: 'templates/Slider.html', controller: 'sliderController'
+   }).
+
+when('/accordion', {
+      templateUrl: 'templates/quickaccordion.html', controller: 'accordionController'
+   }).
+
+when('/accordion/#panel1a', {
+      templateUrl: 'templates/accordion1.html', controller: 'accordionController'
+   }).
+
+
+when('/accordion/#panel2a', {
+      templateUrl: 'templates/accordion2.html', controller: 'accordionController'
+   }).
+
+when('/accordion/#panel2a', {
+      templateUrl: 'templates/accordion3.html', controller: 'accordionController'
+   }).
+
+when('/register', {
+      templateUrl: 'templates/register.html', controller: 'registerController'
+   }).
+
    otherwise({
       redirectTo: 'templates/login.html'
    });
